78efeb2d83d6dd9b5c907bc5a57b05a24a477594
Erik Schnetter <schnetter@gmail.com>
2018-09-28 14:20:23 -0400

    Simpliy path name

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,12 @@ using Test
 
 using DataAndPlotting
 
-arr = initialize()
-rm("/tmp/array.h5")
-output(arr, "/tmp/array.h5")
-# for Windows: output(arr, "/temp/array.h5")
+filename = "array.h5"
 
-arr2 = input("/tmp/array.h5")
+arr = initialize()
+rm(filename, force=true)
+output(arr, filename)
+
+arr2 = input(filename)
 
 @test isequal(arr2, arr)
